f5ba4ed391fdac9ec89ee3dad04262d8e45ee630
mimi-2023 <karasu@LAPTOP-L90LNOLE>
2024-06-13 00:01:17 +0900

    テーブルクラス作成、alembicによるマイグレーション実施 #2

---
+++ backend/database.py
@@ -0,0 +1,25 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+MYSQL_USER = os.getenv("MYSQL_USER")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
+MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
+MYSQL_HOST = os.getenv("MYSQL_HOST")
+MYSQL_PORT = os.getenv("MYSQL_PORT")
+
+ASYNC_DB_URL = f"mysql+aiomysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DATABASE}"
+
+async_engine = create_async_engine(ASYNC_DB_URL, echo=True)
+async_session = sessionmaker(
+    autocommit=False, autoflush=False, bind=async_engine, class_=AsyncSession
+)
+
+Base = declarative_base()
+
+async def get_db():
+    async with async_session() as session:
+        yield session
+
+# alembicは非同期でも大丈夫？？→非同期エンジン使用に書き換えないといけない。
+# あと、dockerではどうするのかな？
