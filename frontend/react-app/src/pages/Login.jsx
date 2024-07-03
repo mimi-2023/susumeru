ca7d702d39aff6cceeeb50eedce1670ca2f9b2ec
mimi-2023 <karasu@LAPTOP-L90LNOLE>
2024-07-04 00:57:03 +0900

    Loginの見た目作成 #16

--- frontend/react-app/src/pages/Login.jsx
+++ frontend/react-app/src/pages/Login.jsx
@@ -1,5 +1,42 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-    return <h1>login page</h1>;
+  return (
+    <div className="bg-myPaleBlue text-textBlack font-roundedMplus font-medium min-h-screen">
+      <div className="mx-auto pt-20 w-5/6 max-w-xs">
+        <h1 className="mb-10 text-xl text-center font-semibold">
+          ログイン
+        </h1>
+        <form action="" className="space-y-10">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">
+              メールアドレス
+            </label>
+            <input id="email" type="text" placeholder="  Email" className="rounded-lg shadow-lg text-lg"/>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">
+              パスワード
+            </label>
+            <input id="password" type="password" placeholder="  Password" className="rounded-lg shadow-lg text-lg"/>
+          </div>
+          <div className="flex justify-center">
+            <button 
+              type="submit" 
+              className="w-[140px] h-[40px] font-semibold rounded-lg bg-myOrange hover:bg-hoverOrange focus:ring-2 focus:ring-textBlack"
+              >
+              ログイン
+            </button>
+          </div>  
+        </form>
+        <div className="flex justify-center mt-10">
+          <Link className="hover:text-textGray">
+              新規登録はこちら
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login;
--- frontend/react-app/tailwind.config.js
+++ frontend/react-app/tailwind.config.js
@@ -5,7 +5,24 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roundedMplus: ['"M PLUS Rounded 1c"', "sans-serif"],
+      },
+    colors: {
+      myPaleBlue: "#BAE2F8",
+      myMediumBlue: "#35A1DC",
+      myDeepBlue: "#006DD2",
+      myOrange: "#FFB800",
+      myRed: "#FF2F2F",
+      myGreen: "#4ECB71",
+      myPaleGray: "#D9D9D9",
+      textBlack: "#404040",
+      textGray: "#5C5C5C",
+      hoverOrange: "#EEAB00"
+
+      },
+    },
   },
   plugins: [],
 }
